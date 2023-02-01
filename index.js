939317d630a80fec27778d54e5540c06b9174a48
Jesse Hunter <jjsbizarreadventure@yahoo.com>
2023-02-01 12:02:32 -0600

    commit tree d388eee2a4e3babebcc7637fda05c7aa5c976c41

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42)
+}
+
+// if (blocks > 42) {
+// return blocks - 42
+// } else (blocks < 42) {
+// return 42 - blocks
+
+
+
+
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264
+}
+
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const fareFeet = distanceTravelledInFeet(startBlock, endBlock)
+    if (fareFeet <= 400) {
+       return 0
+    } else if (fareFeet > 400 && fareFeet <= 2000) {
+       return (fareFeet - 400) * 0.02
+    } else if (fareFeet > 2000 && fareFeet <= 2500) {
+       return 25
+    } else if (fareFeet > 2500) {
+       return 'cannot travel that far'
+    }
+}
+
